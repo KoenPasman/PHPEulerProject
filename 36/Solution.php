7863df5cbfeae72569dd71483aa428a2662d81ed
Koen Pasman <koen@wenstafel.nl>
2014-04-22 22:23:00 +0200

    Adding solution for problem #36

---
+++ 36/Solution.php
@@ -0,0 +1,30 @@
+<?php
+/*************************************************************************
+ * Project Euler Problem 36 (http://projecteuler.net/problem=36)
+ *
+ * A PHP implementation by Koen Pasman
+ * http://koenpasman.nl
+ *************************************************************************/
+
+class Solution implements SolutionInterface
+{
+
+    public function solve()
+    {
+        $found = array();
+        for($i=10; $i<1000000; $i++)
+        {
+            if($this->isPalindrome($i) && $this->isPalindrome(decbin($i)))
+            {
+                $found[] = $i;
+            }
+        }
+
+        return 'The sum of palindromic numbers in base 10 and base 2 is: ' . array_sum($found);
+    }
+
+    public function isPalindrome($number)
+    {
+        return (string) $number == (string) strrev($number);
+    }
+}
