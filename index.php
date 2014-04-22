7e2fdd77a058965c716f16dc73e8bfdd74f8424f
Koen Pasman <koen@wenstafel.nl>
2014-04-22 17:50:14 +0200

    Adding a simple list of solved problems

--- index.php
+++ index.php
@@ -5,13 +5,29 @@ require_once('SolutionInterface.php');
 $problem = isset($_GET['problem']) && ctype_digit($_GET['problem']) ? $_GET['problem'] : null;
 
 ini_set('max_execution_time', 180);
+ini_set('Display_errors', 'on');
+error_reporting(E_ALL ^ E_NOTICE);
 
-if($problem != null && is_dir($problem)) 
+if($problem == null)
 {
-	
-	if(file_exists($problem . '/Solution.php')) 
+    // Show small index
+    $dirList = glob('[0-9]*', GLOB_ONLYDIR);
+    sort($dirList, SORT_NUMERIC);
+
+    echo '<h1>Problem overview</h1>';
+    echo '<ul>';
+    foreach($dirList as $dir) {
+        echo '<li><a href="./?problem=' . $dir . '">Problem #' . $dir . '</a>';
+    }
+    echo '</ul>';
+}
+else if($problem != null && is_dir($problem))
+{
+    $solutionFile = $problem . '/Solution.php';
+	if(file_exists($solutionFile))
 	{
-		require_once($problem . '/Solution.php');
+		require_once($solutionFile);
+
 
 		$solution = new Solution();
 
