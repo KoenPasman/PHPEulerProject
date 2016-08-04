2be2a6631b0721758a3a9e307586d9ba63994899
Koen Pasman <koen@aubergine-it.nl>
2016-08-04 16:37:49 +0200

    Fixing Problem 17: use 'forty' instead of 'fourty' to get the correct answer. Also refactored some code

--- 17/Solution.php
+++ 17/Solution.php
@@ -13,7 +13,7 @@ class Solution implements SolutionInterface
     // Define words, zero is not used but needed in the array
     private $singles = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
     private $tenToTwenty = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-    private $tenFolds = ['twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    private $tenFolds = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
     private $connector = 'and';
     private $hundred = 'hundred';
@@ -26,40 +26,53 @@ class Solution implements SolutionInterface
 
         for ($i = 1; $i <= 1000; $i++) {
             // Build the string for the current number
-            if ($i < 100) {
-                $current = $this->belowHundred($i);
-            } else if ($i < 1000) {
-                $current = $this->singles[(int) floor($i / 100)] . $this->hundred;
-                if ($i % 100 > 0) {
-                    $current .= $this->connector . $this->belowHundred($i % 100);
-                }
-            } else {
-                // We don't need to take numbers > 1000 into account, so this will suffice
-                $current = $this->singles[1] . $this->thousand;
-            }
+            $current = $this->pronounceNumber($i);
 
             // Count the number of characters, and add it to the total number of characters
             $totalSum += strlen($current);
         }
 
-        echo '<p>Total amount of characters: ' . $totalSum;
+        echo '<p>Total amount of characters: ' . $totalSum . '</p>';
+    }
+
+    /**
+     * Create a pronounceable string of a certain number.
+     *
+     * @param $number
+     * @return string
+     */
+    private function pronounceNumber($number)
+    {
+        if ($number < 100) {
+            $current = $this->belowHundred($number);
+        } else if ($number < 1000) {
+            $current = $this->singles[(int) floor($number / 100)] . $this->hundred;
+            if ($number % 100 > 0) {
+                $current .= $this->connector . $this->belowHundred($number % 100);
+            }
+        } else {
+            // We don't need to take numbers > 1000 into account, so this will suffice
+            $current = $this->singles[1] . $this->thousand;
+        }
+
+        return $current;
     }
 
     /**
      * Returns a string representation of any number between 1 and 99 (1 and 99 included).
      *
-     * @param int $i The integer that is converted
+     * @param int $number The integer that is converted
      * @return string
      **/
-    private function belowHundred($i)
+    private function belowHundred($number)
     {
-        if ($i <= 0) return null;
-        if ($i < 10) {
-            return $this->singles[$i];
-        } else if ($i < 20) {
-            return $this->tenToTwenty[$i % 10];
-        } else if ($i < 100) {
-            return $this->tenFolds[(int) floor($i / 10) - 2] . $this->singles[$i % 10];
+        if ($number <= 0) return null;
+        if ($number < 10) {
+            return $this->singles[$number];
+        } else if ($number < 20) {
+            return $this->tenToTwenty[$number % 10];
+        } else if ($number < 100) {
+            return $this->tenFolds[(int) floor($number / 10) - 2] . $this->singles[$number % 10];
         }
         return null;
     }
