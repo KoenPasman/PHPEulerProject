03ac2e1e4c0b529ee702eba759a360befff771d5
Koen Pasman <koen@aubergine-it.nl>
2016-08-04 16:48:26 +0200

    Boilerplate for a solution fix

---
+++ 59/Solution.php
@@ -0,0 +1,16 @@
+<?php
+
+/*************************************************************************
+ * Project Euler Problem 59 (http://projecteuler.net/problem=59)
+ *
+ * A PHP implementation by Koen Pasman
+ * http://koenpasman.nl
+ *************************************************************************/
+
+class Solution implements SolutionInterface
+{
+    public function solve()
+    {
+        echo 'Nothing here yet..';
+    }
+}
